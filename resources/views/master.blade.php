11a8e61b9bc331e96b88e4ce1a57a4c1f24c3a78
Niels van de Pas <nielsvandepas@live.nl>
2016-01-15 18:35:47 +0100

    Updated licence information

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -21,6 +21,8 @@
 			@yield('main')
 		</main>
 
+		<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
+
 		<script src="/js/script.js"></script>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
