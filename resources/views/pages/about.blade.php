2319292050cff325daba2867b48e1b6f8341e0a7
Niels van de Pas <nielsvandepas@live.nl>
2016-01-15 19:30:10 +0100

    Added licence information

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -12,4 +12,8 @@
 	<p>Jaap is the lead researcher. He is also responsible for all design except the website.</p>
 	<h3>Niels</h3>
 	<p>Niels is responsible for this website, front-end, back-end and design.</p>
+
+	<h3>Licence</h3>
+	<p>This site is licensed under the Creative Commons BY-NC-SA 4.0 licence. Basically, this means that you may take and use any part of this site, as long as you mention our names, don't use it commercially and you share it under the same CC BY-NC-SA licence.</p>
+	<p><strong>BUT!</strong> you may not use the content that was published on this site by our users, without their explicit approval.</p>
 @stop
