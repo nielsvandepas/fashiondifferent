891992ba59c812f74f9c61219228407fe5963f5c
Niels van de Pas <nielsvandepas@live.nl>
2016-01-18 22:13:47 +0100

    Fixed role pivot table

--- database/migrations/2016_01_18_142956_CreateRolePivotTable.php
+++ database/migrations/2016_01_18_142956_CreateRolePivotTable.php
@@ -12,7 +12,7 @@ class CreateRolePivotTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('role-user', function(Blueprint $table)
+		Schema::create('role_user', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->integer('user_id')->unsigned();
@@ -36,7 +36,7 @@ class CreateRolePivotTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('role-user');
+		Schema::drop('role_user');
 	}
 
 }
