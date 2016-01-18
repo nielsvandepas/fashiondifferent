04ab0cd1947daa55273e2b9cbfd9ecb7796cfa53
Niels van de Pas <nielsvandepas@live.nl>
2016-01-18 22:13:14 +0100

    Made start for administration panel

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,12 @@
+<?php namespace FashionDifferent\Http\Controllers;
+
+use FashionDifferent\Http\Requests;
+use FashionDifferent\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller {
+
+	//
+
+}
